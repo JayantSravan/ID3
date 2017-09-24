6800baa23b1d075c823d9a23a68b5524d0fbfee5
JayantSravan <jayant.sravan@gmail.com>
2017-09-24 15:28:04 +0530

    Started something

---
+++ node.java
@@ -0,0 +1,18 @@
+import java.util.*
+
+class Node
+{
+  ArrayList<ArrayList<String>> reducedTable = new ArrayList<ArrayList<String>>();
+
+  String classifyingAttribute;
+  ArrayList<Node> childNodes = new ArrayList<Node>();
+  float entropy;
+
+  private float calcEntropy()
+  {
+    for(  )
+    {
+
+    }
+  }
+}
