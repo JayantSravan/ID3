e219d2bb498c186b9fb8df1da5fa8951fa156740
SUBHADIP JANA <subhadip1997a@gmail.com>
2017-09-27 11:09:33 +0530

    changes

--- node.java
+++ node.java
@@ -1,18 +1,44 @@
-import java.util.*
+package ID3;
+import java.util.ArrayList;
 
-class Node
+public class Node
 {
-  ArrayList<ArrayList<String>> reducedTable = new ArrayList<ArrayList<String>>();
-
-  String classifyingAttribute;
-  ArrayList<Node> childNodes = new ArrayList<Node>();
-  float entropy;
-
-  private float calcEntropy()
-  {
-    for(  )
-    {
-
-    }
-  }
+	private String nodeName;
+	private ArrayList<Node> children = new ArrayList<>();
+	private ArrayList<Integer> dataList_positive;
+	private ArrayList<Integer> dataList_negative;
+	
+	public Node(String nodeName, ArrayList<Integer> dataList)
+	{
+		this.nodeName = nodeName;
+		for(Integer index : dataList)
+		{
+			if()
+		}
+	}
+	
+	public ArrayList<Node> getChildren()
+	{
+		return children;
+	}
+	public void setNodeName(String name)
+	{
+		this.nodeName = name;
+	}
+	public String getNodeName()
+	{
+		return nodeName;
+	}
+	public void addChildren(Node child)
+	{
+		children.add(child);
+	}
+	public ArrayList<Integer> getDataListPositive()
+	{
+		return dataList_positive;
+	}
+	public ArrayList<Integer> getDataListNegative()
+	{
+		return dataList_negative;
+	}
 }
