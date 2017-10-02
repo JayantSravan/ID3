8ab8d509614c583fc9c5f8b37b2d46923b49d4a5
JayantSravan <jayant.sravan@gmail.com>
2017-10-02 18:20:38 +0530

    Pruning added

--- NewDriver.java
+++ NewDriver.java
@@ -304,6 +304,96 @@ public class NewDriver
  	}
 
 
+	///////////////////////////////////////for Pruning ///////////////////////////////////////////////////////
+	public static pruneTree(Node root) //driver method for pruning.
+	{
+		/*
+		1. Iterate over the tree multiple times. Stop when pruning can no longer increase the accuracy.
+		2. In each iteration, make that node a leaf and check accuracy.
+		3. Each time, mark the node with the maximum accuracy and prune from that node.
+		4. Continue this till no node increases the accuracy of the tree.
+		 */
+		boolean iterateCondition = true;
+		double accuracy = findAccuracy(root);
+		while(iterateCondition)
+		{
+			Node nodeToBePruned = findBestLeafNode(root); //////gets the node which on pruning gives best accuracy
+			nodeToBePruned.leafBit=1;   //make the best one a leaf
+			double maxAccuracy = findAccuracy(root);
+			if(maxAccuracy > accuracy) ///checking if pruning is any better
+			{
+				accuracy = maxAccuracy;
+			}
+			else
+			{
+				iterateCondition = false; //to break out in the next iteration
+				nodeToBePruned.leafBit = 0; //revert back the changes made
+			}
+		}
+	}
+	static Node best = root;
+	static double maxAccuracy;
+	public static Node findBestLeafNode( Node root )
+	{
+		maxAccuracy = 0; //maximum accuracy that can be achieved after pruning the present tree
+		DFS(root); //traverse the entire tree to know the best node to be used
+		return  best;
+	}
+
+	public static void DFS (Node node)
+	{
+		if(node.leafBit == 1) //return if this node itself is a leaf
+		{
+			return 0;
+		}
+
+		//*****************************************
+		//check if this node gives the best accuracy
+		node.leafBit = 1; //make it a leaf
+		node.leafValue = findMostCommonOutput( node ); //check the majority output of the node
+		double accuracy = findAccuracy(root); //get the accuracy if this node is made a leaf
+		if(accuracy>maxAccuracy)
+		{
+			best = node; //make this the best node
+			maxAccuracy = accuracy;//make this the best accuracy
+		}
+		node.leafBit = 0; //make the node an internal node again
+		//*****************************************
+
+
+		for(Node child : node.children) //repeat the process for its children also
+		{
+			DFS(child);
+		}
+	}
+
+	public static boolean findMostCommonOutput( Node node)  //Computes if the dataset in a node says YES or NO if it were a leaf
+	{
+		int i=0;   //counters
+		int j=0;
+		for(ArrayList<String> datapoint : node.reducedDataSet)  //traverse through and count
+		{
+			String output = datapoint.get(lastIndexOf(datapoint));
+			if(output.equals("1"))
+			{
+				i++;
+			}
+			else
+			{
+				j++;
+			}
+		}
+		if(i>=j)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	//////////////////////////////////////end - for pruning////////////////////////////////////////////////////
+
 
 
 
